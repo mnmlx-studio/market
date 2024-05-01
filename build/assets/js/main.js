6e92d3ea37e927a634101f873d1fc0c59986f35b
k.ushakov <paxbit.mobile@gmail.com>
2024-05-01 21:56:46 +0300

    new pages

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -1,4 +1,34 @@
-"use strict";
+'use strict';
 
-document.addEventListener("DOMContentLoaded", function () {});
+document.addEventListener("DOMContentLoaded", function () {
+    $('.range').each(function () {
+        var _min = $(this).data('min');
+        var _max = $(this).data('max');
+        var _step = $(this).data('step');
+
+        $(this).slider({
+            range: true,
+            min: _min,
+            max: _max,
+            step: _step,
+            values: [_max * 0.2, _max * 0.7]
+        });
+    });
+
+    var swiper = new Swiper('.category-swiper', {
+        slidesPerView: 1,
+        effect: "fade",
+        pagination: {
+            el: ".swiper-pagination",
+            type: "fraction",
+            renderFraction: function renderFraction(a, b) {
+                return '<span class="' + a + '"></span>' + '&nbsp;&nbsp;&mdash;&nbsp;&nbsp;' + '<span class="' + b + '"></span>';
+            }
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+        }
+    });
+});
 //# sourceMappingURL=main.js.map
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+    $('.range').each(function() {
+        let _min = $(this).data('min');
+        let _max = $(this).data('max');
+        let _step = $(this).data('step');
+
+        $(this).slider({
+            range: true,
+            min: _min,
+            max: _max,
+            step: _step,
+            values: [_max * 0.2, _max * 0.7],
+        });
+    })
+
+    const swiper = new Swiper('.category-swiper', {
+        slidesPerView: 1,
+        effect: "fade",
+        pagination: {
+            el: ".swiper-pagination",
+            type: "fraction",
+	        renderFraction: function(a,b) {
+	        	return '<span class="' + a + '"></span>' + '&nbsp;&nbsp;&mdash;&nbsp;&nbsp;' + '<span class="' + b + '"></span>';
+	        }
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+});
